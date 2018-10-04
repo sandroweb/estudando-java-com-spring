920cf7bd53b0b597494f86be1e0524a2c8273a36
Sandro Santos <sandro@sandrosantos.art.br>
2018-10-03 22:41:10 -0300

    App 2 - Criando um app Spring utilizando o New -> Spring Starter Project. Versão do Spring Boot 2.0.5

---
+++ app-2-cursomc/src/main/java/br/art/sandrosantos/java/app2cursomc/App2CursomcApplication.java
@@ -0,0 +1,12 @@
+package br.art.sandrosantos.java.app2cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App2CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App2CursomcApplication.class, args);
+	}
+}
---
+++ app-2-cursomc/src/test/java/br/art/sandrosantos/java/app2cursomc/App2CursomcApplicationTests.java
@@ -0,0 +1,16 @@
+package br.art.sandrosantos.java.app2cursomc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class App2CursomcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
