7aac7863f29af3c988a4898402ab64079d87bf05
Sandro Santos <sandro@sandrosantos.art.br>
2018-10-03 23:43:17 -0300

    App 2 - Implementação do Swagger no path localhost:8080/swagger-ui.html#/

--- app-2-cursomc/src/main/java/br/art/sandrosantos/java/app2cursomc/App2CursomcApplication.java
+++ app-2-cursomc/src/main/java/br/art/sandrosantos/java/app2cursomc/App2CursomcApplication.java
@@ -3,7 +3,10 @@ package br.art.sandrosantos.java.app2cursomc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class App2CursomcApplication {
 
 	public static void main(String[] args) {
