7ecf633d528910accf880abf15fb4227aa8773c2
Sandro Santos <sandro@sandrosantos.art.br>
2018-10-06 16:32:31 -0300

    App 2 - Criando operação de instanciação.

--- app-2-cursomc/src/main/java/br/art/sandrosantos/java/app2cursomc/App2CursomcApplication.java
+++ app-2-cursomc/src/main/java/br/art/sandrosantos/java/app2cursomc/App2CursomcApplication.java
@@ -1,15 +1,35 @@
 package br.art.sandrosantos.java.app2cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.art.sandrosantos.java.app2cursomc.domain.Categoria;
+import br.art.sandrosantos.java.app2cursomc.repositories.CategoriaRepository;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class App2CursomcApplication {
+public class App2CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(App2CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 }
