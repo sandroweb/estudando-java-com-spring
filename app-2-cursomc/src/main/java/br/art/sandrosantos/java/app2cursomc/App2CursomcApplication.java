40b4d718498b27254edea66ac8ed3992f6cec56e
Sandro Santos <sandro@sandrosantos.art.br>
2018-10-06 17:33:25 -0300

    App 2 - Associação muitos para muitos.

--- app-2-cursomc/src/main/java/br/art/sandrosantos/java/app2cursomc/App2CursomcApplication.java
+++ app-2-cursomc/src/main/java/br/art/sandrosantos/java/app2cursomc/App2CursomcApplication.java
@@ -8,15 +8,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.art.sandrosantos.java.app2cursomc.domain.Categoria;
+import br.art.sandrosantos.java.app2cursomc.domain.Produto;
 import br.art.sandrosantos.java.app2cursomc.repositories.CategoriaRepository;
+import br.art.sandrosantos.java.app2cursomc.repositories.ProdutoRepository;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
 public class App2CursomcApplication implements CommandLineRunner {
-	
+
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(App2CursomcApplication.class, args);
@@ -28,8 +33,20 @@ public class App2CursomcApplication implements CommandLineRunner {
 
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		
+
+		Produto prod1 = new Produto(null, "Computador", 2000.00);
+		Produto prod2 = new Produto(null, "Impressora", 800.00);
+		Produto prod3 = new Produto(null, "Mouse", 80.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(prod1, prod2, prod3));
+		cat2.getProdutos().addAll(Arrays.asList(prod2));
+
+		prod1.getCategorias().addAll(Arrays.asList(cat1));
+		prod2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		prod3.getCategorias().addAll(Arrays.asList(cat1));
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(prod1, prod2, prod3));
 		
 	}
 }
--- app-2-cursomc/src/main/java/br/art/sandrosantos/java/app2cursomc/domain/Produto.java
+++ app-2-cursomc/src/main/java/br/art/sandrosantos/java/app2cursomc/domain/Produto.java
@@ -13,8 +13,8 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 @Entity
-public class Categoria implements Serializable {
-
+public class Produto implements Serializable {
+	
 	private static final long serialVersionUID = 1L;
 
 	@Id
@@ -22,19 +22,26 @@ public class Categoria implements Serializable {
 	private Integer id;
 	
 	private String nome;
+	
+	private Double preco;
+	
+	@ManyToMany
+	@JoinTable(name = "PRODUTO_CATEGORIA",
+		joinColumns = @JoinColumn( name = "produto_id" ),
+		inverseJoinColumns = @JoinColumn( name = "categoria_id" )	
+	)
+	private List<Categoria> categorias = new ArrayList<Categoria>();
 
-	@ManyToMany(mappedBy="categorias")
-	private List<Produto> produtos = new ArrayList<Produto>();
-
-	public Categoria() {
+	public Produto() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public Categoria(Integer id, String nome) {
+	public Produto(Integer id, String nome, Double preco) {
 		super();
 		this.id = id;
 		this.nome = nome;
+		this.preco = preco;
 	}
 
 	public Integer getId() {
@@ -53,16 +60,20 @@ public class Categoria implements Serializable {
 		this.nome = nome;
 	}
 
-	public List<Produto> getProdutos() {
-		return produtos;
+	public Double getPreco() {
+		return preco;
 	}
 
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
+	public void setPreco(Double preco) {
+		this.preco = preco;
 	}
 
-	public static long getSerialversionuid() {
-		return serialVersionUID;
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
 	}
 
 	@Override
@@ -81,7 +92,7 @@ public class Categoria implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Produto other = (Produto) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -90,4 +101,8 @@ public class Categoria implements Serializable {
 		return true;
 	}
 	
+	
+	
+	
+
 }
